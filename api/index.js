b4d94d84670c0055318779953e98105f0af240ee
himnxhu <himanxhuupadhyay@gmail.com>
2026-06-25 15:44:33 +0530

    Migrate to Vercel zero-config directory structure

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use(helmet({
 }));
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
-app.use(express.static(path.join(__dirname, "static")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 const signupSchema = z.object({
   name: z.string().trim().min(2).max(80),
@@ -537,7 +537,7 @@ app.use("/api", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "static", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 app.use((error, req, res, next) => {
